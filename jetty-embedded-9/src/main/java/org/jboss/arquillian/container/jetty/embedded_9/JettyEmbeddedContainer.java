53c07143708ad3b906572cc01b571f32505b5312
Aslak Knutsen <aslak@4fs.no>
2015-08-20 00:15:26 +0200

    Update to Arquillian Core 1.1.8.Final

--- jetty-embedded-9/src/main/java/org/jboss/arquillian/container/jetty/embedded_9/JettyEmbeddedContainer.java
+++ jetty-embedded-9/src/main/java/org/jboss/arquillian/container/jetty/embedded_9/JettyEmbeddedContainer.java
@@ -137,9 +137,8 @@ public class JettyEmbeddedContainer implements DeployableContainer<JettyEmbedded
         try
         {
             server = new Server();
-
             // Use default configuration classes at the server level
-            ClassList serverConf = ClassList.serverDefault(server);
+            ClassList serverConf = ClassList.setServerDefault(server);
 
             String configuredConfigurationClasses = containerConfig.getConfigurationClasses();
             if (configuredConfigurationClasses != null && configuredConfigurationClasses.trim().length() > 0)
