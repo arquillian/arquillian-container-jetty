38a562379eb854cc31391e754cf0e793fdf8e35a
Scott M Stark <starksm64@gmail.com>
2022-02-17 21:31:53 -0600

    Rever the 3.1 change as core has not added a protocol specific to the servlet 3.1 API. 3.0 is the closest that exists.
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- jetty-embedded-9/src/main/java/org/jboss/arquillian/container/jetty/embedded_9/JettyEmbeddedContainer.java
+++ jetty-embedded-9/src/main/java/org/jboss/arquillian/container/jetty/embedded_9/JettyEmbeddedContainer.java
@@ -126,7 +126,7 @@ public class JettyEmbeddedContainer implements DeployableContainer<JettyEmbedded
     public ProtocolDescription getDefaultProtocol() {
         // Jetty 9 is a Servlet 3.1 container.
         // However, Arquillian "Protocol" actuall means "Packaging"
-        return new ProtocolDescription("Servlet 3.1");
+        return new ProtocolDescription("Servlet 3.0");
     }
 
     public void setup(JettyEmbeddedConfiguration containerConfig) {
